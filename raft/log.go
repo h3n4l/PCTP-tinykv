b846e97d1a78a46805674641e2b518dd3e2abdd6
zhaosuyang <859382989@qq.com>
2021-12-13 16:43:33 +0800

    20211213 log

--- raft/log.go
+++ raft/log.go
@@ -14,7 +14,12 @@
 
 package raft
 
-import pb "github.com/pingcap-incubator/tinykv/proto/pkg/eraftpb"
+import (
+	"errors"
+	pb "github.com/pingcap-incubator/tinykv/proto/pkg/eraftpb"
+)
+
+var EntriesUnavailable = errors.New("[RaftLog]: Requested entry at index is unavailable")
 
 // RaftLog manage the log entries, its struct look like:
 //
@@ -69,23 +74,70 @@ func (l *RaftLog) maybeCompact() {
 // unstableEntries return all the unstable entries
 func (l *RaftLog) unstableEntries() []pb.Entry {
 	// Your Code Here (2A).
-	return nil
+	if l.isEntriesEmpty() {
+		return []pb.Entry{}
+	}
+	if l.stabled == l.LastIndex() {
+		return []pb.Entry{}
+	}
+	offset := l.entries[0].Index
+	ents := make([]pb.Entry, l.LastIndex()-l.stabled)
+	for k, ent := range l.entries[l.stabled-offset+1:] {
+		ents[k] = ent
+	}
+	return ents
 }
 
 // nextEnts returns all the committed but not applied entries
 func (l *RaftLog) nextEnts() (ents []pb.Entry) {
 	// Your Code Here (2A).
-	return nil
+	if l.isEntriesEmpty() {
+		return []pb.Entry{}
+	}
+	if l.committed == l.applied {
+		return []pb.Entry{}
+	}
+	offset := l.entries[0].Index
+	ents = make([]pb.Entry, l.committed-l.applied)
+	for k, ent := range l.entries[l.applied-offset+1 : l.committed-offset+1] {
+		ents[k] = ent
+	}
+	return ents
 }
 
 // LastIndex return the last index of the log entries
 func (l *RaftLog) LastIndex() uint64 {
 	// Your Code Here (2A).
-	return 0
+	if l.isEntriesEmpty() {
+		//先进性持久化再进行压缩 当日志被压缩的时候 日志为空时候 stabled记录的就是压缩的最后一条日志
+		return l.stabled
+	}
+	return l.entries[len(l.entries)-1].Index
 }
 
 // Term return the term of the entry in the given index
 func (l *RaftLog) Term(i uint64) (uint64, error) {
 	// Your Code Here (2A).
-	return 0, nil
+	if i == 0 {
+		return 0, nil
+	}
+	if l.isEntriesEmpty() {
+		return 0, EntriesUnavailable
+	}
+	offset := l.entries[0].Index
+	if i > l.LastIndex() || i < offset {
+		return 0, EntriesUnavailable
+	}
+	return l.entries[i-offset].Term, nil
+}
+
+func (l *RaftLog) isEntriesEmpty() bool {
+	return len(l.entries) == 0
+}
+
+func (l *RaftLog) append(ents ...pb.Entry) {
+	if len(ents) == 0 {
+		return
+	}
+	l.entries = append(l.entries, ents...)
 }
